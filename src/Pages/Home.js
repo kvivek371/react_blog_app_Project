51251466df470f7f346a19c9f17e9b4442e16348
kvivek371 <kvivek371@gmail.com>
2022-06-07 13:40:31 +0530

    react_blog_app

---
+++ src/Pages/Home.js
@@ -0,0 +1,156 @@
+import React,{useContext} from 'react'
+import { Link } from 'react-router-dom'
+import Card from '../Component/Card'
+import CardHome from '../Component/CardHome'
+import SmallCard from '../Component/SmallCard'
+import { store } from './Details'
+
+const Home = () => {
+
+  const [detail, setDetail] = useContext(store);
+  return (
+
+    <div>
+    <div  className='Home'>
+      
+        <div className="home__container">
+      <div className="home__first">
+        <h1>Kerala : Gods own Land</h1>
+      </div>
+      <div className="home__sec">
+       <Link to="/fitness" > <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201911/barbell-3704578_960_720.jpeg?oFwa7w4RGXJrvmB9T64Zy1xTmhdChjAc&size=770:433" alt="not found" /></Link>
+       
+      </div>
+      </div>
+      <div className="home__thir">
+       <Link to='/technology'>   <img src="https://www.investopedia.com/thmb/QvLIdxSBRTqN0PFDFfW0YV9ZA70=/2121x1193/smart/filters:no_upscale()/GettyImages-964033964-ca3290057ccc4024b57e755423572264.jpg" alt="not found" /></Link>
+          </div>
+          <h1 style={{marginTop:"40px"}}>The Latest</h1>
+          <hr style={{width:"200px",  thickness:"20px"}} />
+          <div className='home__left left1'>
+                    {
+                        detail.filter((article) => { return article.category === "bollywood" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+          <div className='home__left left1'>
+                    {
+                        detail.filter((article) => { return article.category === "technology" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+          <div className='home__left left1'>
+                    {
+                        detail.filter((article) => { return article.category === "food" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+                <h1 style={{marginTop:"40px", display : "inline-block"}}>Latest Articles</h1>
+                
+                <h1 className='Top' >Top Posts</h1>
+                <hr style={{width:"200px",  thickness:"20px",}} />
+                
+
+                <div className='rightbar2'>
+                    {
+                        detail.filter((article) => { return article.category === "mix" }).map((n) => (
+                            <Card
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+                <div className="sidebar2">
+                    {
+                        detail.filter((article) => { return article.category === "mix" }).map((n) => (
+                            <SmallCard
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                description={n.description.slice(0, 200)}
+                                title={n.title.slice(0, 25)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+
+                    <div className='advertisement'>
+                       <p>Advertisement</p>
+                    </div>
+                </div>
+                <h1 style={{marginTop:"40px", display : "inline-block"}}>Latest Stories</h1>
+                <hr />
+
+                <div className='home__left'>
+                    {
+                        detail.filter((article) => { return article.category === "footer1" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='home__left'>
+                    {
+                        detail.filter((article) => { return article.category === "footer2" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+
+                <div className='home__left'>
+                    {
+                        detail.filter((article) => { return article.category === "footer3" }).map((n) => (
+                            <CardHome
+                                articleid={n.id}
+                                imgUrl={n.Image}
+                                title={n.title}
+                                description={n.description.slice(0, 200)}
+                                author={n.author}
+                            />
+                        ))
+                    }
+                </div>
+                    
+    </div>
+  
+    </div>
+  )
+}
+
+export default Home
